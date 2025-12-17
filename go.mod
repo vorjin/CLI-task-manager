4b5edbaf58a428898d22cb6b2ecd5b6df05d931d
Dmytro Varhan <vorjin6272@gmail.com>
2025-12-17 22:02:04 +0200

    correctly identify user's home dir

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.25.1
 
 require github.com/spf13/cobra v1.10.2 // direct
 
-require github.com/boltdb/bolt v1.3.1
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/mitchellh/go-homedir v1.1.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
--- main.go
+++ main.go
@@ -4,13 +4,20 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"github.com/mitchellh/go-homedir"
 	"stask/cmd"
 	"stask/db"
 )
 
 func main() {
-	var path = "task-manager.db"
-	err := db.BoltDBInit(path)
+	var path = `/task-manager.db`
+	osDir, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+	dbPath := osDir + path
+
+	err = db.BoltDBInit(dbPath)
 	if err != nil {
 		panic(err)
 	}
